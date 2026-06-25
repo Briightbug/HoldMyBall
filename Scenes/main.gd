7372885462fbc3ef39af8485928f5be373d79db3
Briightbug <briightbuggamedev@gmail.com>
2026-06-25 11:23:07 +0200

    Made ball spin, try to implement timer

--- Scenes/main.gd
+++ Scenes/main.gd
@@ -1,21 +1,14 @@
-class_name Ball extends RigidBody3D
+extends Node3D
 
-var timer : int = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	
+	
+	
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	
-	
-	
 	pass
-
-# Handles input events
-func _input(event):
-	if event is InputEventMouseButton:
-		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
-			print("Left Mouse pressed!")
